4a0e465682260c5b20eef9a0e7c8a424407c2fde
Andreas Lande <andreas.lande@medisin.uio.no>
2022-01-12 14:04:43 +0100

    Added another session of data
    + Added another session to data

---
+++ ProjectData/Tutorial_Spatial_Tuning/Metadata Tables/+tablevar/+session/DataIsDownloaded.m
@@ -0,0 +1,16 @@
+function value = DataIsDownloaded(sessionObject)
+%DATAISDOWNLOADED Get value for DataIsDownloaded
+%   Detailed explanation goes here
+    
+    % Initialize output value with the default value.
+    value = false;                 % Please do not edit this line
+    
+    % Return default value if no input is given (used during config).
+    if nargin < 1; return; end	% Please do not edit this line
+    
+    
+    % Insert your code here:
+    
+    
+end
+
